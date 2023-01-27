71b4c7fcaf6cde54a54fcf0a0a998ada9f7ff58d
lucavivenzo <84680310+lucavivenzo@users.noreply.github.com>
2023-01-27 18:06:01 +0100

    Add files via upload

---
+++ progetto/src/test/java/com/sad/progetto/ProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sad.progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
