a521f1b7cbd518329960fe43bec31086cd03541e
xSimoss <simone-staiano@libero.it>
2023-01-29 16:08:30 +0100

    commit 2

--- progetto/src/main/java/com/sad/progetto/ProgettoApplication.java
+++ progetto/src/main/java/com/sad/progetto/ProgettoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProgettoApplication {
 
-	//prova luca vivenzo merge
+	//prova luca vivenzo mergeeee
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgettoApplication.class, args);
